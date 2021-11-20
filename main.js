cf3165392a2d121d14f7b60c797cc0dcb284b10b
WaiHChan <chanwai@pdx.edu>
2021-11-20 02:53:22 -0800

    final touch up

--- main.js
+++ main.js
@@ -2,8 +2,27 @@ let form = document.getElementById("form");
 form.style.border = "thin solid #040404";
 form.style.textAlign = "center";
 form.style.width = "50%";
+form.style.height = "100%";
 form.style.borderRadius = "10px";
-form.style.padding = "2%";
+form.style.padding = "4%";
 form.style.marginLeft = "auto";
 form.style.marginRight = "auto";
 form.style.fontFamily = "Open Sans, Helvetica Neue, sans-serif";
+form.style.marginBottom = "100px";
+
+let labels = document.getElementsByTagName("label");
+for (label of labels) {
+    label.style.fontSize = "30px";
+}
+
+let name = document.getElementById("name");
+name.style.height = "50px";
+
+let email = document.getElementById("email");
+email.style.height = "50px";
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+}
