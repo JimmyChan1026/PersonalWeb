6ccdf994b4a24dc1d08b391d7b92b8c799264c79
WaiHChan <chanwai@pdx.edu>
2021-11-20 16:03:49 -0800

    fixing bug

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+// let se_work = document.getElementById("work");
+// se_work.style.backgroundColor = "#717171";
+
+// let se_work_h1 = document.getElementById("work").querySelector("h1");
+// se_work_h1.style.color = "#fcfcfc";
+
+// let se_contact = document.getElementById("contact");
+// se_contact.style.backgroundColor = "#717171";
+
+// let se_contact_h1 = document.getElementById("contact").querySelector("h1");
+// se_contact_h1.style.color = "#fcfcfc";
+
+
 let form = document.getElementById("form");
 form.style.border = "thin solid #040404";
 form.style.textAlign = "center";
@@ -9,14 +22,16 @@ form.style.marginLeft = "auto";
 form.style.marginRight = "auto";
 form.style.fontFamily = "Open Sans, Helvetica Neue, sans-serif";
 form.style.marginBottom = "100px";
+form.style.backgroundColor = "#a8a8a8";
 
 let labels = document.getElementsByTagName("label");
 for (label of labels) {
+    label.style.color = "#fcfcfc";
     label.style.fontSize = "30px";
 }
 
-let name = document.getElementById("name");
-name.style.height = "50px";
+let n = document.getElementById("name");
+n.style.height = "50px";
 
 let email = document.getElementById("email");
 email.style.height = "50px";
